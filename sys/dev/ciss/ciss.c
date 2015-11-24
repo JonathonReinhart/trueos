b409aa9e0cbc4b15ea6f5236b4d3d93885ba7a7f
Jonathon Reinhart <jreinhart@cc-sw.com>
2015-11-24 15:16:52 -0500

    ciss: add xpt_path_(un)lock in ciss_name_device()
    Reconcile the differences between:
    
    FreeBSD "master" d85167d0:
    Author: sbruno <sbruno@FreeBSD.org>
    Date:   Fri Apr 11 20:19:01 2014 +0000
    
        Fix insta-panic on assert of unlocked periph mtx in ciss(4) when
        logical volume state changes.
    
        Currently, I view this as a critical fix for users and will MFC this rapidly as
        my testing has shown data loss when the disk is failed by removing it when
        under some amount of write activity and this code panics the box.
    
        Reviewed by:        mav@ scottl@
        MFC after:  3 days
        Sponsored by:       Yahoo! Inc.
    
    and
    
    TrueOS 9.3.1-STABLE 34cc27f9:
    Author: sbruno <sbruno@FreeBSD.org>
    Date:   Tue Apr 15 17:52:22 2014 +0000
    
        MFC r264354 but change the lock arg and implementation to fit the current state
        of CAM in stable/9
    
        Fix insta-panic on assert of unlocked periph mtx in ciss(4) when
        logical volume state changes.
    
        I'm still setting the mergeinfo as done here as the intent of the MFC, if not
        the EXACT code is being implemented.
    
        Reviewed by:        mav@
        Sponsored by:       Yahoo! Inc

--- sys/dev/ciss/ciss.c
+++ sys/dev/ciss/ciss.c
@@ -3438,7 +3438,9 @@ ciss_name_device(struct ciss_softc *sc, int bus, int target)
 			     target, 0);
 
     if (status == CAM_REQ_CMP) {
+	xpt_path_lock(path);
 	periph = cam_periph_find(path, NULL);
+	xpt_path_unlock(path);
 	xpt_free_path(path);
 	if (periph != NULL) {
 		sprintf(sc->ciss_logical[bus][target].cl_name, "%s%d",
